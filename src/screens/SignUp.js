8da920d4f7a5666902f86c36b83b6a7d39946931
Lokesh <lokesh.loki.codes@gmail.com>
2024-04-19 23:12:09 +0530

    image picker added

--- src/screens/SignUp.js
+++ src/screens/SignUp.js
@@ -21,6 +21,7 @@ import { Options } from "../utils/Options";
 import propTypes from "prop-types";
 import { signUp } from "../action/auth";
 import { connect } from "react-redux";
+import { options } from "../../icon master/lcorn-master/socialApp/src/utils/options";
 
 const SignUp = ({signUp}) => {
     
@@ -30,13 +31,50 @@ const SignUp = ({signUp}) => {
     const [instaUserName, setInstaUserName] = useState('');
     const [country, setCountry] = useState('');
     const [bio, setBio] = useState('');
-    const [image, setimage] = useState('.../icon master/lcorn-master/smlogo.png');
-    const [imageUploading, setImageeUploading] = useState(false);
+    const [image, setImage] = useState('.../icon master/lcorn-master/smlogo.png');
+    const [imageUploading, setImageUploading] = useState(false);
     const [uploadStatus, setUploadStatus]= useState(null);
     
+
+    const chooseImage = async () => {
+        ImagePicker.showImagePicker(options, (response) => {console.log('Response = ', response)
+        })
+
+        if (response.didCancel) { console.log('User cancelled image picker');
+        } else if (response.error) { console.log('ImagePicker Error: ', response.error);
+        } else if (response.customButton) { console.log('User tapped custom button: ', response.customButton);
+        } else { 
+            console.log(response)              // or uri.data
+            uploadImage(response)
+         };
+    }
+
+    const uploadImage = async (response) => {
+        setImageUploading(true)
+        const reference = storage().ref(response.fileName)
+
+        const task = reference.putFile(response.path)
+        task.on("state_changed", (taskSnapshot) => {
+            const percentage = (taskSnapshot.bytesTransferred / taskSnapshot.totalBytes) * 1000
+
+            setUploadStatus(percentage)
+        })
+        
+        task.then(async () => {
+            const url = await reference.getDownloadURL()
+
+            setImage(url)
+            setImageUploading(false)
+        })
+    }
+
+    const doSignUp = async () => {
+        signUp({name, instaUserName, bio, country, email, password})
+    }
+
     return(
         <>
-
+            <Text>Hi from Loki</Text>
         </>
     )
 }
