b6036d1ea6bea4f4fd43fa8db9c24bac24f98ee9
Lokesh <lokesh.loki.codes@gmail.com>
2024-04-21 00:52:16 +0530

    firebase

--- index.js
+++ index.js
@@ -8,3 +8,13 @@ import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => RootApp);
 
+// const functions = require('firebase-functions');
+
+// exports.makeUppercase = functions.firestore.document('/messages/{documentId}')
+//     .onCreate((snap, context) => {
+//       const original = snap.data().original;
+//       console.log('Uppercasing', context.params.documentId, original);
+//       const uppercase = original.toUpperCase();
+//       return snap.ref.set({uppercase}, {merge: true});
+//     });
+
